be87e1ee8228f0e25ece3503ce5917a54558e8f6
kefer <morafi.1999.16@gmail.com>
2023-03-04 11:45:02 -0500

    se agrega conexion a base de datos SQL azure

--- index.ts
+++ index.ts
@@ -1,17 +1,35 @@
 import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
+import sql, { ConnectionPool } from "mssql";
+import dotenv from "dotenv";
+import { DbConfig } from "./credenciales";
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(cors());
+const config: DbConfig = {
+	server: process.env.DB_SERVER ?? "",
+	database: process.env.DB_NAME ?? "",
+	user: process.env.DB_USER ?? "",
+	password: process.env.DB_PASSWORD ?? "",
+	port: Number(process.env.DB_PORT) ?? 0,
+};
 
-app.get("/", (req, res) => {
-	res.send("¡Hola, mundo!");
+async function connect(config: DbConfig): Promise<ConnectionPool> {
+	const pool = await sql.connect(config);
+	return pool;
+}
+app.get("/", async (req, res) => {
+	try {
+		const pool = await connect(config);
+		const result = await pool.request().query("SELECT * FROM usuario");
+		res.json(result.recordset);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Error retrieving data from database");
+	}
 });
 
-app.listen(port, () => {
-	console.log(`Servidor iniciado en http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`);
 });
