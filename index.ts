889486bd793bf5aac1f71cadb1104d80e5a932a5
kefer <morafi.1999.16@gmail.com>
2023-03-07 23:48:22 -0500

    se implementa swagger openapi 3.0.0

--- index.ts
+++ index.ts
@@ -1,17 +1,39 @@
 import express from "express";
 import bodyParser from "body-parser";
-
 import dotenv from "dotenv";
+import * as fs from "fs";
+import swaggerUi from "swagger-ui-express";
 import { sequelize } from "./src/config/conexion";
 import { usuarioRoutes } from "./src/routes/usuario.routes";
 import { autenticacionRoutes } from "./src/routes/autentication.routes";
 
 dotenv.config();
 
-const app = express();
+// Cargar la especificación Swagger desde un archivo JSON
+const swaggerDocument = JSON.parse(fs.readFileSync("./swagger.json", "utf-8"));
 
-// Middleware
+// Definir la URL base a partir de la variable de entorno
+const baseUrl = process.env.API_HOST || "http://localhost:3000";
+// Start server and connect to database
+const port = process.env.PORT || 3000;
+// Agregar la URL base a la especificación Swagger
+swaggerDocument.servers = [{ url: baseUrl }];
+
+// Crear una instancia de express
+const app = express();
 app.use(bodyParser.json());
+
+// Configurar swagger-ui-express con la especificación y la URL base
+app.use("/api-docs", swaggerUi.serve);
+app.get(
+	"/api-docs",
+	swaggerUi.setup(swaggerDocument, {
+		swaggerOptions: { url: `${baseUrl}/swagger.json` },
+	})
+);
+
+// // Configuración de Swagger
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use("/usuario", usuarioRoutes);
@@ -25,13 +47,12 @@ app.get("/authenticated/example", (req, res) => {
 	return res.json({ message: "Authenticated" });
 });
 
-// Start server and connect to database
-const port = process.env.PORT || 3000;
-
 sequelize
 	.authenticate()
 	.then(() => {
 		console.log("Connected to database");
-		app.listen(port, () => console.log(`Server running on port ${port}`));
+		app.listen(port, () =>
+			console.log(`Server running on port ${port} + ${process.env.API_HOST}`)
+		);
 	})
 	.catch((error) => console.log(error));
